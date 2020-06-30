998139ac0c20e12431274c1fba40b30abaa8f3a6
Justin <jlmartin432@gmail.com>
2020-06-29 19:42:50 -0700

    added 3 sec attack/ decay envelope

--- src/Flower.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Flower extends React.Component {
-    componentDidMount() {
-
-    }
-}
-
-export default Flower;
--- src/SynthCanvas.js
+++ src/SynthCanvas.js
@@ -1,38 +1,83 @@
 import React from 'react';
 import sunflower from './static/sunflower.png';
-import Flower from './Flower';
-import Audio from './Audio';
-import SynthBoard from './SynthBoard'
 
 class SynthCanvas extends React.Component {
 
     constructor(props){
         super(props);
         this.notes = [];
+        this.audioCtx = this.props.audioContext;
+        this.masterGain = this.audioCtx.createGain();
+        this.masterGain.connect(this.audioCtx.destination)
+        this.masterGain.gain.setValueAtTime(1.0, this.audioCtx.currentTime)
         this.canvasRef = React.createRef();
         this.image = new Image();
     }
 
+    createWaveForm () {
+        let real = new Float32Array(12);
+        let imag = new Float32Array(12);
+
+        real[0] = 0;
+        real[1] = 0.4;
+        real[2] = 0.4;
+        real[3] = 1;
+        real[4] = 1;
+        real[5] = 1;
+        real[6] = 0.3;
+        real[7] = 0.7;
+        real[8] = 0.6;
+        real[9] = 0.5;
+        real[10] = 0.9;
+        real[11] = 0.8;
+
+        // imag[0] = Math.random();
+        // imag[1] = Math.random();
+        // imag[2] = Math.random();
+        // imag[3] = Math.random();
+        // imag[4] = Math.random();
+        // imag[5] = Math.random();
+        // imag[6] = Math.random();
+        // imag[7] = Math.random();
+        // imag[8] = Math.random();
+        // imag[9] = Math.random();
+        // imag[10] = Math.random();
+        // imag[11] = Math.random();
+
+
+        return [real, imag];
+    }
+
     playNote = function(pitchScalar, lengthScalar, speed) {
-        const context = this.props.audioContext;
-        const masterGain = context.createGain()
-        masterGain.connect(context.destination)
-        const now = context.currentTime
-        const oscillator = context.createOscillator()
-        oscillator.type = 'sawtooth'
+        const now = this.audioCtx.currentTime
+        const envelope = this.audioCtx.createGain()
+        envelope.gain.setValueAtTime(0.0, now)
+        envelope.gain.linearRampToValueAtTime(1.0, now + 0.010)
+        //gain.gain.linearRampToValueAtTime(0.0, now + (lengthScalar / 10) / speed)
+        envelope.gain.linearRampToValueAtTime(0.0, now + 3)
+
+        const oscillator = this.audioCtx.createOscillator();
+        let wave;
+        let waveParts = new Promise ( (resolve, reject) => {
+            let w = this.createWaveForm();
+            resolve(w);
+        });
+
+        waveParts.then(result => {
+            wave = this.audioCtx.createPeriodicWave(result[0], result[1]);
+            oscillator.setPeriodicWave(wave);
+        }, function(err) {
+            console.log(err);
+        });
+        oscillator.frequency.setValueAtTime(pitchScalar * 5000, now)
+        //oscillator.connect(this.audioCtx.destination);
+        //oscillator.type = 'sine'
+        oscillator.connect(envelope).connect(this.audioCtx.destination);
         oscillator.start(now)
 
-        const gain = context.createGain()
-        gain.connect(masterGain)
-        oscillator.connect(gain)
-
-        oscillator.frequency.setValueAtTime(pitchScalar * 5000, now)
-
-        gain.gain.setValueAtTime(0.0, now)
-        gain.gain.setValueAtTime(0.7, now + .010)
-        gain.gain.linearRampToValueAtTime(0.0, now + (lengthScalar / 50) / speed)
-        oscillator.stop(now + (lengthScalar / 50) / speed);
-        return {gain, oscillator}
+        //oscillator.stop(now + (lengthScalar / 50) / speed);
+        oscillator.stop(now + 3);
+        return {envelope, oscillator}
     }
 
     componentDidMount () {
